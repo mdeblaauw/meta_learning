e5c335bf50aa1eb275c97d1cd5c65c7ae9266666
EC2 Default User <ec2-user@ip-172-16-42-28.eu-west-1.compute.internal>
2020-01-12 21:43:11 +0000

    change folder structure

---
+++ few_shot/meta_learning/train_iterators/protonet.py
@@ -0,0 +1,125 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from copy import deepcopy
+from config import EPSILON
+
+def pairwise_distances(x: torch.Tensor,
+                       y: torch.Tensor,
+                       matching_fn: str,
+                       S = None) -> torch.Tensor:
+    """Efficiently calculate pairwise distances (or other similarity scores) between
+    two sets of samples.
+    # Arguments
+        x: Query samples. A tensor of shape (n_x, d) where d is the embedding dimension
+        y: Class prototypes. A tensor of shape (n_y, d) where d is the embedding dimension
+        matching_fn: Distance metric/similarity score to compute between samples
+    """
+    n_x = x.shape[0]
+    n_y = y.shape[0]
+
+    if matching_fn == 'l2':
+        distances = (
+                x.unsqueeze(1).expand(n_x, n_y, -1) -
+                y.unsqueeze(0).expand(n_x, n_y, -1)
+        ).pow(2).sum(dim=2)
+        return distances
+    elif matching_fn == 'gaussian':
+        distances = ((
+                x.unsqueeze(1).expand(n_x, n_y, -1) -
+                y.unsqueeze(0).expand(n_x, n_y, -1)
+        ).pow(2) * S.unsqueeze(0).expand(n_x, n_y,-1)).sum(dim=2)
+    
+        distances = distances.sqrt()
+        return distances
+    elif matching_fn == 'gaussian_v2':
+        difference = x.unsqueeze(1).expand(n_x, n_y, -1) - y.unsqueeze(0).expand(n_x, n_y, -1)
+        difference_two = torch.matmul(S.unsqueeze(0).expand(n_x, n_y,-1,-1), difference.unsqueeze(3))
+        distances = torch.matmul(difference.unsqueeze(2), difference_two).squeeze()
+        distances = distances.sqrt()
+        return distances
+    elif matching_fn == 'cosine':
+        normalised_x = x / (x.pow(2).sum(dim=1, keepdim=True).sqrt() + EPSILON)
+        normalised_y = y / (y.pow(2).sum(dim=1, keepdim=True).sqrt() + EPSILON)
+
+        expanded_x = normalised_x.unsqueeze(1).expand(n_x, n_y, -1)
+        expanded_y = normalised_y.unsqueeze(0).expand(n_x, n_y, -1)
+
+        cosine_similarities = (expanded_x * expanded_y).sum(dim=2)
+        return 1 - cosine_similarities
+    elif matching_fn == 'dot':
+        expanded_x = x.unsqueeze(1).expand(n_x, n_y, -1)
+        expanded_y = y.unsqueeze(0).expand(n_x, n_y, -1)
+
+        return -(expanded_x * expanded_y).sum(dim=2)
+    else:
+        raise(ValueError('Unsupported similarity function'))
+        
+def compute_prototypes(support: torch.Tensor, k: int, n: int) -> torch.Tensor:
+    """Compute class prototypes from support samples.
+    # Arguments
+        support: torch.Tensor. Tensor of shape (n * k, d) where d is the embedding
+            dimension.
+        k: int. "k-way" i.e. number of classes in the classification task
+        n: int. "n-shot" of the classification task
+    # Returns
+        class_prototypes: Prototypes aka mean embeddings for each class
+    """
+    # Reshape so the first dimension indexes by class then take the mean
+    # along that dimension to generate the "prototypes" for each class
+    class_prototypes = support.reshape(k, n, -1).mean(dim=1)
+    return class_prototypes
+
+def proto_net_episode(model,optimiser,loss_fn,x,y,n_shot,k_way,q_queries,distance,train):
+    """Performs a single training episode for a Prototypical Network.
+    # Arguments
+        model: Prototypical Network to be trained.
+        optimiser: Optimiser to calculate gradient step
+        loss_fn: Loss function to calculate between predictions and outputs. Should be cross-entropy
+        x: Input samples of few shot classification task
+        y: Input labels of few shot classification task
+        n_shot: Number of examples per class in the support set
+        k_way: Number of classes in the few shot classification task
+        q_queries: Number of examples per class in the query set
+        distance: Distance metric to use when calculating distance between class prototypes and queries
+        train: Whether (True) or not (False) to perform a parameter update
+    # Returns
+        loss: Loss of the Prototypical Network on this task
+        y_pred: Predicted class probabilities for the query set on this task
+    """
+    if train:
+        # Zero gradients
+        model.train()
+        optimiser.zero_grad()
+    else:
+        model.eval()
+
+    # Embed all samples
+    embeddings = model(x)
+
+    # Samples are ordered by the NShotWrapper class as follows:
+    # k lots of n support samples from a particular class
+    # k lots of q query samples from those classes
+    support = embeddings[:n_shot*k_way]
+    queries = embeddings[n_shot*k_way:]
+    prototypes = compute_prototypes(support, k_way, n_shot)
+
+    # Calculate squared distances between all queries and all prototypes
+    # Output should have shape (q_queries * k_way, k_way) = (num_queries, k_way)
+    distances = pairwise_distances(queries, prototypes, distance)
+
+    # Calculate log p_{phi} (y = k | x)
+    log_p_y = (-distances).log_softmax(dim=1)
+    loss = loss_fn(log_p_y, y)
+
+    # Prediction probabilities are softmax over distances
+    y_pred = (-distances).softmax(dim=1)
+
+    if train:
+        # Take gradient step
+        loss.backward()
+        optimiser.step()
+    else:
+        pass
+
+    return {'loss':loss}, y_pred
---
+++ few_shot/proto_experiment.py
@@ -0,0 +1,123 @@
+import argparse
+
+import torch
+from torch.optim import Optimizer
+from torch.nn import Module
+from torch.utils.data import DataLoader
+from torch import nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import StepLR
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import numpy as np
+
+from meta_learning.models.models import get_few_shot_encoder
+from meta_learning.train_iterators.protonet import *
+from meta_learning.core import *
+from meta_learning.train import fit
+from meta_learning.callbacks import *
+from meta_learning.data_processing.samplers import *
+from meta_learning.data_processing.datasets import OmniglotDataset
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device("cuda" if USE_CUDA else "cpu")
+
+def Initialise_training(args):
+    background = OmniglotDataset('background', args.data_dir, unzip=True)
+    background_taskloader = DataLoader(
+        background,
+        batch_sampler=NShotTaskSampler(
+            background,
+            args.episodes_per_epoch,
+            args.n_train,
+            args.k_train,
+            args.q_train
+        ),
+        num_workers=4
+    )
+
+    evaluation = OmniglotDataset('evaluation', args.data_dir, unzip=True)
+    evaluation_taskloader = DataLoader(
+        evaluation,
+        batch_sampler=NShotTaskSampler(
+            evaluation,
+            args.evaluation_episodes,
+            args.n_test,
+            args.k_test,
+            args.q_test
+        ),
+        num_workers=4
+    )
+
+    model = get_few_shot_encoder(
+        args.num_input_channels
+    ).to(device, dtype=torch.double)
+
+    optimiser = Adam(model.parameters(), lr=args.learning_rate)
+    loss_fn = torch.nn.NLLLoss()
+
+    callbacks = [VerboseLogger()]
+
+    fit(
+        model,
+        optimiser,
+        loss_fn,
+        epochs=args.epochs,
+        dataloader=background_taskloader,
+        prepare_batch=prepare_nshot_task(args.n_train, args.k_train, args.q_train),
+        metrics=['categorical_accuracy'],
+        callbacks=callbacks,
+        verbose=1,
+        fit_function=proto_net_episode,
+        fit_function_kwargs={'n_shot':args.n_train,
+                            'k_way':args.k_train,
+                            'q_queries':args.q_train,
+                            'distance':args.distance,
+                            'train':True}
+    )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--distance", type=str,
+                        default="l2",
+                        help="")
+    parser.add_argument("--n-train", type=int, default=5,
+                        help="")
+    parser.add_argument("--k-train", type=int, default=5,
+                        help="")
+    parser.add_argument("--q-train", type=int, default=5,
+                        help="")
+    parser.add_argument("--n-test", type=int, default=5,
+                        help="")
+    parser.add_argument("--k-test", type=int, default=5,
+                        help="")
+    parser.add_argument("--q-test", type=int, default=5,
+                        help="")
+    parser.add_argument("--evaluation-episodes", type=int, default=20,
+                        help="")
+    parser.add_argument("--episodes-per-epoch", type=int, default=5,
+                        help="")
+    parser.add_argument("--final-evaluation-episodes", type=int, default=5,
+                        help="")
+    parser.add_argument("--epochs", type=int, default=5,
+                        help="")
+    parser.add_argument("--dataset-name", type=str, default='Omniglot',
+                        help="")
+    parser.add_argument("--num-input-channels", type=int, default=1,
+                        help="")
+    parser.add_argument("--learning-rate", type=float, default=0.001,
+                        help="")
+    
+    
+    # Container environment
+    # parser.add_argument('--hosts', type=list, default=json.loads(os.environ['SM_HOSTS']))
+    # parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
+    # parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    # parser.add_argument('--num-gpus', type=int, default=os.environ['SM_NUM_GPUS'])
+    
+    # parser.add_argument('--data-dir', type=str)
+
+    args = parser.parse_args()
+    Initialise_training(args)
